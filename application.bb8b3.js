cfee9d1ea0c46c84e9eedd69e6c7f1a708815c1c
Boosh-2023 <aaron@harborfi.io>
2024-10-18 21:47:18 +0800

    3.8.4 version8

--- application.bb8b3.js
+++ application.bb8b3.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.08577.json';
+          this.settingsPath = 'src/settings.34fb2.json';
           this.showFPS = false;
         }
         init(engine) {
